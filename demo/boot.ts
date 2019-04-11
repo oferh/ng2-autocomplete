372e3039a13e5c4ff50b2214746f2ca714e43c0f
André Werlang <beppe85@gmail.com>
2019-04-11 17:17:28 -0300

    Report observable leaks in demo project

--- demo/boot.ts
+++ demo/boot.ts
@@ -1,12 +1,24 @@
 "use strict";
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Observable } from 'rxjs';
+import { setup, track, printSubscribers } from 'observable-profiler';
 
 import { AppModule } from './app-module';
 
 if ("production" === ENV) {
-  // Production
-  enableProdMode();
+    // Production
+    enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+setup(Observable);
+platformBrowserDynamic().bootstrapModule(AppModule).then((ref) => {
+    track();
+    (window as any).stopProfiler = () => {
+        ref.destroy();
+        const subscribers = track(false);
+        printSubscribers({
+            subscribers,
+        });
+    };
+});
