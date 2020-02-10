3866ea3f763511f207cb58c92a6fa18850dde838
Martin Donadieu <martindonadieu@gmail.com>
2020-02-10 14:50:16 +0100

    make demo work

--- demo/app-cmp.ts
+++ demo/app-cmp.ts
@@ -1,13 +1,10 @@
 "use strict";
 import { Component } from "@angular/core";
 
-// tslint:disable-next-line: no-var-requires
-const template = require("./app-cmp.html");
-
 @Component({
     // tslint:disable-next-line: component-selector
     selector: "demo-app",
-    template
+    templateUrl: "./app-cmp.html"
 })
 export class AppComponent {
     public isCollapsed = false;
